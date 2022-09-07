6536c314a360af99897ee84a56f73108dc8450eb
Avinash Singh <rajputavinash124@gmail.com>
2022-09-07 17:30:09 +0530

    changes

--- extech/settings.py
+++ extech/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'django-insecure-!jfuu18+$+sgojj$%joy-_zf=8(e!4od8+kiugrk_j*zh2jzcc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.0']
+ALLOWED_HOSTS = ['127.0.0.0','extechdigital.herokuapp.com']
 
 
 EMAIL_HOST = 'smtp.mailtrap.io'
