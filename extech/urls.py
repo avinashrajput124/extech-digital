eafa929aa66491dbc1763d5a1be93bffc2fa8203
Avinash Singh <rajputavinash124@gmail.com>
2022-09-09 16:57:05 +0530

    url_path error

--- extech/urls.py
+++ extech/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from django.views.static import serve
-from django.conf.urls import url
-from django.conf import settings
 
+from django.conf import settings
+from django.urls import re_path as url
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include("extechapp.urls")),
